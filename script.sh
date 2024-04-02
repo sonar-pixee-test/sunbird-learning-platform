0d46c4f8189bee56a21f179961dfd91e304e9429
Nithin-ns24 <99644046+Nithin-ns24@users.noreply.github.com>
2024-04-02 15:45:02 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ cd docs/domain_model_v2/definitions                    #move inside th folder
 
 #*****************************************************
 #Find all files required & copy the content of the file to a variable .user variable in the curl command to definition-update
-for file in *                        
+for file in * * *                        
 do
    variable=`cat $file`
    curl -i -X POST -H "Content-Type: application/json" -H "user-id: ansible_user" -d "$variable" http://54.254.225.115:8080/learning-service/taxonomy/domain/definition
